c2477c2d08c9a365a8fbabdb74b6fbcaff518ab6
Plamen Prangov <plamenprangov@gmail.com>
2015-10-26 01:18:29 +0200

    Create Status.cs

---
+++ MorskiShah/Logic/Status.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MorskiShah.Logic;
+
+namespace MorskiShah.Logic
+{
+    public
+    enum
+    Status
+    {
+        Xuser, Ouser
+    }
+}
